52bcc48edcf371c480aa3d25ce73121fb98b8468
Jan Pecha <janpecha@email.cz>
2016-06-26 10:58:27 +0200

    bootstrap: added detection of debugMode via ENV (for Docker,...)
    See https://forum.nette.org/cs/26561-configurator-a-detekce-prostredi-v-dockeru#p176044

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,6 +5,10 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
+if (($debug = getenv('NETTE_DEBUG')) !== FALSE) { // https://forum.nette.org/cs/26561-configurator-a-detekce-prostredi-v-dockeru#p176044
+	$configurator->setDebugMode($debug === '1');
+}
+
 //$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
 
